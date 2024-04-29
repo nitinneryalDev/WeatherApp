a88940b31b5ee64a62300010e0387f693fa8c31e
nitinneryalDev <nitinneryal@gmaile.com>
2024-04-29 22:47:17 +0530

    styling changes

--- index.js
+++ index.js
@@ -8,19 +8,16 @@ let w_icon = document.querySelector(".weather_icon");
 let w_minTem = document.querySelector(".weather_min");
 let w_maxTem = document.querySelector(".weather_max");
 
-
 let w_feelsLike = document.querySelector(".weather_feelsLike");
 let w_humidity = document.querySelector(".weather_humidity");
 let w_wind = document.querySelector(".weather_wind");
 let w_pressure = document.querySelector(".weather_pressure");
-
 
 const getCountryName = (code) => {
   return new Intl.DisplayNames([code], { type: "region" }).of(code);
 };
 
 const getDateTime = (dt) => {
-
   const curDate = new Date(dt * 1000);
   const options = {
     weekday: "long",
@@ -31,32 +28,28 @@ const getDateTime = (dt) => {
     minute: "numeric",
   };
 
-const formatter = new Intl.DateTimeFormat('en-US' , options );
-return formatter.format(curDate)
-
-
+  const formatter = new Intl.DateTimeFormat("en-US", options);
+  return formatter.format(curDate);
 };
 
-https: getWeatherData = async () => {
+ getWeatherData = async () => {
   const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=shimla&appid=b07720ce80f388d0a34c37eb6621ddaf`;
 
   try {
     const res = await fetch(weatherUrl);
     const data = await res.json();
-
     const { main, name, weather, wind, sys, dt } = data;
     cityName.innerHTML = `${name} , ${getCountryName(sys.country)} `;
     dateTime.innerHTML = getDateTime(dt);
     w_temperature.innerHTML = `${main.temp}&#176`;
     w_minTem.innerHTML = `Min: ${main.temp_min.toFixed()}&#176`;
     w_maxTem.innerHTML = `Max: ${main.temp_max.toFixed()}&#176`;
-    w_icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather[0].icon}@4x.png" />`
-    w_forecast.innerHTML = `${weather[0].main}`
+    w_icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather[0].icon}@4x.png" />`;
+    w_forecast.innerHTML = `${weather[0].main}`;
     w_feelsLike.innerHTML = `${main.feels_like.toFixed(2)}&#176`;
     w_humidity.innerHTML = `${main.humidity}%`;
     w_wind.innerHTML = `${wind.speed}m/s`;
     w_pressure.innerHTML = `${main.pressure} hPa`;
-
     console.log(data);
   } catch (error) {
     console.log(error);
